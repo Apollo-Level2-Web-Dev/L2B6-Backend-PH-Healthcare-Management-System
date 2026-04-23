b3da7baec993a5c184577b9a34a0c7ffa6568d1a
shafayathub <shafayat.ms@gmail.com>
2026-04-23 07:37:51 +0600

    ingestion is on for doctor data. will be completed in next module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -12,22 +12,23 @@ import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { StatsRoutes } from "../module/stats/stats.route";
 import { UserRoutes } from "../module/user/user.route";
+import { RagRoutes } from "../module/rag/rag.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
-router.use("/specialties", SpecialtyRoutes)
-router.use("/users", UserRoutes)
-router.use("/patients", PatientRoutes)
-router.use("/doctors", DoctorRoutes)
-router.use("/admins", AdminRoutes)
-router.use("/schedules", scheduleRoutes)
-router.use("/doctor-schedules", DoctorScheduleRoutes)
-router.use("/appointments", AppointmentRoutes)
-router.use("/prescriptions", PrescriptionRoutes)
-router.use("/reviews", ReviewRoutes)
-router.use("/stats", StatsRoutes)
-router.use("/payments", PaymentRoutes)
-
+router.use("/specialties", SpecialtyRoutes);
+router.use("/users", UserRoutes);
+router.use("/patients", PatientRoutes);
+router.use("/doctors", DoctorRoutes);
+router.use("/admins", AdminRoutes);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
+router.use("/appointments", AppointmentRoutes);
+router.use("/prescriptions", PrescriptionRoutes);
+router.use("/reviews", ReviewRoutes);
+router.use("/stats", StatsRoutes);
+router.use("/payments", PaymentRoutes);
+router.use("/rag", RagRoutes);
 
 export const IndexRoutes = router;
